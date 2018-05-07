819f3a8093e948e0e25b23f1e934e8fab2ef6219
David Zukowski <david@zuko.me>
2018-05-07 15:27:59 -0700

    Feat: stub out layout component

---
+++ src/lib/createComponent.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import hoistNonReactStatics from 'hoist-non-react-statics'
+import { connect, FelaTheme } from 'react-fela'
+import { createShorthandFactory } from './factories'
+import getElementType from './getElementType'
+import getUnhandledProps from './getUnhandledProps'
+
+const createComponent = (Component, config) => {
+  const { Component, rules, variables, shorthand, getDefaultElement } = config
+  const StyledComponent = rules ? connect(rules)(Component) : Component
+
+  const UIComponent = props => (
+    <FelaTheme
+      render={(siteVariables) => {
+        const mergedVariables = variables ? { ...variables(siteVariables), ...props.variables } : props.variables
+        const rest = getUnhandledProps(Component, props)
+        const ElementType = getElementType(Component, props, getDefaultElement)
+
+        return (
+          <StyledComponent
+            {...props}
+            rest={rest}
+            variables={mergedVariables}
+            ElementType={ElementType}
+          />
+        )
+      }}
+    />
+  )
+
+  UIComponent.create = createShorthandFactory(UIComponent, shorthand)
+  UIComponent.displayName = `UI(${Component.displayName || Component.name || 'Anonymous'})`
+
+  return hoistNonReactStatics(UIComponent, Component)
+}
+
+export default createComponent
